9e06e641a6567818cfb090f35be7a8d017a3cca4
VitaliiEv <solomonov.v@gmail.com>
2022-06-29 01:03:45 +0300

    corrected some warnings

--- src/org/itmo/java/homework_port/Main.java
+++ src/org/itmo/java/homework_port/Main.java
@@ -24,6 +24,8 @@ public class Main {
     //    private static final int TASK_CARGO_MAX;
     private static final int TASK_CARGO_MIN;
 
+    private static Random random = new Random();
+
     static {
         PORT_CAPACITY_MAX = 300;
         PORT_CAPACITY_MIN = 100;
@@ -65,8 +67,8 @@ public class Main {
 //        Генерируются случайные задания на погрузку и разгрузку кораблей.
         for (Ship ship : ships) {
             Thread shipThread = new Thread(() -> {
-                long cargoTask1 = (new Random()).nextInt((int) ship.getCargo() - 1) + 1;
-                long cargoTask2 = (new Random()).nextInt((int) ship.capacity - TASK_CARGO_MIN) + TASK_CARGO_MIN;
+                long cargoTask1 = (long) random.nextInt((int) ship.getCargo() - 1) + 1;
+                long cargoTask2 = (long) random.nextInt((int) ship.capacity - TASK_CARGO_MIN) + TASK_CARGO_MIN;
                 System.out.println("Ship №" + ship.getId() + " (" + ship.getStats() + ") task 1: unload " + cargoTask1 + " cargo from ship to port");
                 System.out.println("Ship №" + ship.getId() + " (" + ship.getStats() + ") task 2: load " + cargoTask2 + " cargo from port to ship");
                 ship.dockAt(port);
@@ -113,7 +115,6 @@ public class Main {
     }
 
     public static Port portInitRandom() {
-        Random random = new Random();
         int portCapacity = random.nextInt(PORT_CAPACITY_MAX - PORT_CAPACITY_MIN) + PORT_CAPACITY_MIN;
         int portCargo = random.nextInt(PORT_CARGO_MAX - PORT_CARGO_MIN) + PORT_CARGO_MIN;
         int pierceNum = random.nextInt(PIERCE_NUM_MAX - PIERCE_NUM_MIN) + PIERCE_NUM_MIN;
@@ -164,7 +165,6 @@ public class Main {
     }
 
     public static Ship[] shipInitRandom() {
-        Random random = new Random();
         int shipNum = random.nextInt(SHIP_NUM_MAX - SHIP_NUM_MIN) + SHIP_NUM_MIN;
         int shipCapacity;
         int shipCargo;
--- src/org/itmo/java/homework_port/Port.java
+++ src/org/itmo/java/homework_port/Port.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class Port extends Storage {
 
-    private String id;
     private int pierceNum;
     private int shipsNum = 0;
     private Pierce[] pierceList;
@@ -54,7 +53,7 @@ public class Port extends Storage {
                 setShipsNum(this.shipsNum + 1);
                 ship.setPierce(pierce);
                 pierce.setShip(ship);
-                System.out.println("Ship №" +ship.getId()  + " docked." + getAllStats());
+                System.out.println("Ship №" + ship.getId() + " docked. " + getAllStats());
 
             }
 
@@ -66,7 +65,7 @@ public class Port extends Storage {
     }
 
     public synchronized void unOccupyPierce(Ship ship) {
-        System.out.println("Ship №" + ship.id + " undocked, load:" + ship.getStats());
+        System.out.println("Ship №" + ship.getId() + " undocked, load:" + ship.getStats());
         ship.getPierce().setShip(null);
         ship.setPierce(null);
         setShipsNum(this.shipsNum - 1);
@@ -75,34 +74,39 @@ public class Port extends Storage {
 
     public synchronized Pierce getUnoccupiedPierce() {
         // get unoccupied pierce with max loadSpeed
+
         if (this.shipsNum == this.pierceNum) {
 //        if (Arrays.stream(pierceList).filter(p -> p.getShip() == null).count() == 0) {
             return null;
         } else {
-            return Arrays.stream(pierceList).filter(p -> p.getShip() == null).max(Loader::compareLoadSpeed).get();
+            return Arrays.stream(pierceList)
+                    .filter(p -> p.getShip() == null)
+                    .max(Loader::compareLoadSpeed)
+                    .get();
         }
     }
 
     public synchronized String getAllStats() {
-        String stats = "PORT STATS: ships: " + this.shipsNum + "/" + this.pierceNum + ", port load: " + getStats() + "\n" +
-                "Ship stats: ";
+        StringBuilder stats = new StringBuilder("PORT STATS: ships: " + this.shipsNum + "/" + this.pierceNum +
+                ", port load: " + getStats() + "\n" + "Ship stats: ");
         Pierce p;
         Ship s;
         for (int i = 0; i < this.pierceNum; i++) {
             p = this.pierceList[i];
             s = p.getShip();
+            stats.append("pierce №").append(p.getId());
             if (s == null) {
-                stats += "pierce №" + p.getId() + " unoccupied";
+                stats.append(" unoccupied");
             } else {
-                stats += "pierce №" + p.getId() + " - ship №" + s.getId() + " load " + s.getStats();
+                stats.append(" - ship №").append(s.getId()).append(" load ").append(s.getStats());
             }
             if (i == this.pierceNum - 1) {
-                stats += ".";
+                stats.append(".");
             } else {
-                stats += ", ";
+                stats.append(", ");
             }
         }
-        return stats;
+        return String.valueOf(stats);
     }
 
     private void initPierce() {
