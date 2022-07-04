4619d20689e07b77da77e1047a71be58b7aeeb20
VitaliiEv <solomonov.v@gmail.com>
2022-07-04 20:34:13 +0300

    homework 7 static class

--- src/org/itmo/java/homework7/Plane.java
+++ src/org/itmo/java/homework7/Plane.java
@@ -3,7 +3,7 @@ package org.itmo.java.homework7;
 public class Plane {
 //    private Wing wings;
 
-    public class Wing {
+    private class Wing {
         private double weight;
 
         public Wing(double weight) {
@@ -19,18 +19,39 @@ public class Plane {
         }
     }
 
+    private static class Seat {
+        private int size;
+
+        public Seat(int size) {
+            this.size = size;
+        }
+
+        @Override
+        public String toString() {
+            return "Seat{" +
+                    "size=" + size +
+                    '}';
+        }
+    }
+
     public Wing createWing(double weight) {
         return new Wing(weight);
     }
 
     public static void main(String[] args) {
-        Plane p = new Plane();
+        // instantiate nested non-static class
         Plane.Wing wingLeft = new Plane().createWing(12d);
         Plane.Wing wingRight = new Plane().new Wing(12.5d);
+        Plane p = new Plane();
         Plane.Wing wingRear = p.new Wing(0.5d);
+
         System.out.println(wingLeft.getWeight());
         System.out.println(wingRight.getWeight());
         System.out.println(wingRear.getWeight());
+
+        // instantiate nested static class
+        Plane.Seat seats = new Plane.Seat(300);
+        System.out.println(seats);
     }
 
 }
